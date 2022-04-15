1b7d638097e807ab9e69f9eaabcc061d52f779e6
Cyb3rFake <nevergreen17@gmail.com>
2022-04-15 15:09:30 +0300

    parameters added

--- src/test/java/guru/qa/WebTest.java
+++ src/test/java/guru/qa/WebTest.java
@@ -5,39 +5,33 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.conditions.Text;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static com.codeborne.selenide.Selenide.*;
 
 public class WebTest {
 
-    @DisplayName("Проверка яндекса по слову Selenide")
-    @Test
-    void selenideSearchTest(){
-        // предусловия :
-        Selenide.open("https://ya.ru");
-        // Шаги:
-        //<input class="input__control input__input mini-suggest__input" tabindex="2" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" aria-autocomplete="list" aria-label="Запрос" id="text" maxlength="400" name="text" role="combobox" aria-controls="suggest-list-nbpzg9gpya7">
-        $("#text").setValue("Selenide");
-        //<button class="button mini-suggest__button button_theme_search button_size_search-large i-bem button_js_inited" data-bem="{&quot;button&quot;:{}}" tabindex="-1" role="button" type="submit"><span class="button__text">Найти</span></button>
-        $("button[type='submit']").click();
-        // Ожидаемы результат:
-        $$(".serp-item")
-                .find(Condition.text("Selenide"))
-                .shouldBe(Condition.visible);
-    }
+    //Дата провайдер или аргумент сорс
 
-    @DisplayName("Проверка яндекса по слову jUnit")
-    @Test
-    void jUnitSearchTest(){
+    @ValueSource(strings = {
+            "Selenide",
+            "JUnit"
+    })
+
+    @DisplayName("Проверка яндекса по слову {}")
+    @ParameterizedTest(name ="Проверка яндекса по слову {0}")
+    void yaSearchTest(String testData){
         // предусловия :
         Selenide.open("https://ya.ru");
         // Шаги:
         //<input class="input__control input__input mini-suggest__input" tabindex="2" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" aria-autocomplete="list" aria-label="Запрос" id="text" maxlength="400" name="text" role="combobox" aria-controls="suggest-list-nbpzg9gpya7">
-        $("#text").setValue("jUnit");
+        $("#text").setValue(testData);
         //<button class="button mini-suggest__button button_theme_search button_size_search-large i-bem button_js_inited" data-bem="{&quot;button&quot;:{}}" tabindex="-1" role="button" type="submit"><span class="button__text">Найти</span></button>
         $("button[type='submit']").click();
         // Ожидаемы результат:
         $$(".serp-item")
-                .find(Condition.text("jUnit"))
+                .find(Condition.text(testData))
                 .shouldBe(Condition.visible);
     }
 }
